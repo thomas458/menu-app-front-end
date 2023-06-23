9a1da6ed643f54c3f3eddeb9d2bfdb9502037234
jiaomeizhou <zhoujiaomei@outlook.com>
2023-06-22 22:00:18 -0700

    Add choose role option to Register screen

--- src/users/register.js
+++ src/users/register.js
@@ -4,9 +4,10 @@ import {useDispatch, useSelector} from "react-redux";
 import {registerThunk} from "./users-thunk";
 
 const Register = () => {
-    const[username, setUsername] = useState('alice')
-    const[password, setPassword] = useState('alice123')
-    const[validatePassword, setValidatePassword] = useState('alice123')
+    const [username, setUsername] = useState('alice')
+    const [password, setPassword] = useState('alice123')
+    const [type, setType] = useState('USER')
+    const [validatePassword, setValidatePassword] = useState('alice123')
     const [error, setError] = useState(null)
     const {currentUser} = useSelector((state) => state.users)
     const dispatch = useDispatch()
@@ -24,32 +25,42 @@ const Register = () => {
         // }
 
     }
-    return(
+    return (
         <>
             <Nav/>
             <h1>Register</h1>
+            <h2>Create an account</h2>
             {
                 error &&
                 <div className="alert alert-danger">
                     {error}
                 </div>
             }
-
+            <label>Username</label>
             <input
                 className="form-control mb-2"
-                value={username}
+                // value={username}
                 onChange={(e) => setUsername(e.target.value)}/>
+            <label>Password</label>
             <input
                 className="form-control mb-2"
-                value={password}
+                // value={password}
                 onChange={(e) => setPassword(e.target.value)}/>
+            <label>Verify your password</label>
             <input
                 className="form-control mb-2"
-                value={validatePassword}
+                // value={validatePassword}
                 onChange={(e) => setValidatePassword(e.target.value)}/>
+            <label>Choose your role</label>
+            <select className="form-select">
+                <option selected>User</option>
+                <option>Premium Member</option>
+                onChange={(e) => setType(e.target.value)}/>
+            </select>
+
             <button
                 onClick={handleRegisterBtn}
-                className="btn btn-primary w-100">
+                className="btn btn-primary w-100 mt-3">
                 Register
             </button>
             {
