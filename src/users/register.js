92c7803eef1857d0b5d67842a2b99a040d6053f8
jiaomeizhou <zhoujiaomei@outlook.com>
2023-06-25 00:39:36 -0700

    Pop error: user doesn't fill all fields

--- src/users/register.js
+++ src/users/register.js
@@ -6,10 +6,10 @@ import {useNavigate} from "react-router";
 import {Navigate} from "react-router-dom";
 
 const Register = () => {
-    const [username, setUsername] = useState('alice')
-    const [password, setPassword] = useState('alice123')
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
     const [type, setType] = useState('USER')
-    const [validatePassword, setValidatePassword] = useState('alice123')
+    const [validatePassword, setValidatePassword] = useState()
     const [error, setError] = useState(null)
     const {currentUser} = useSelector((state) => state.users)
     const dispatch = useDispatch()
@@ -19,12 +19,17 @@ const Register = () => {
             setError('Passwords must match')
             return
         }
+        if (!username || !password || !validatePassword) {
+            setError("Please fill in all fields");
+            return;
+        }
         setError(null)
         const newUser = {username, password, type}
 
         try {
             await dispatch(registerThunk(newUser));
             navigate("/profile");
+            console.log(username, password, type);
         } catch (e) {
             alert(e);
         }
@@ -45,23 +50,23 @@ const Register = () => {
                     {error}
                 </div>
             }
-            <label for="username">Username</label>
+            <label htmlFor="username">Username</label>
             <input id="username" placeholder="Enter your username"
                    className="form-control mb-2"
                 // value={username}
                    onChange={(e) => setUsername(e.target.value)}/>
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input id="password" type="password" placeholder="Enter your password"
                    className="form-control mb-2"
                 // value={password}
                    onChange={(e) => setPassword(e.target.value)}/>
-            <label for="password2">Confirm password</label>
+            <label htmlFor="password2">Confirm password</label>
             <input id="password2" type="password" placeholder="Confirm your password"
                    className="form-control mb-2"
                 // value={validatePassword}
                    onChange={(e) => setValidatePassword(e.target.value)}/>
-            <label>Choose your role</label>
-            <select className="form-select" value={type} onChange={(e) => setType(e.target.value)}>
+            <label htmlFor="role">Choose your role</label>
+            <select id="role" className="form-select" value={type} onChange={(e) => setType(e.target.value)}>
                 <option value="USER">USER</option>
                 <option value="PREMIUM">PREMIUM</option>
             </select>
