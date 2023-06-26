0cf73844f036fe8c09c2be0df85392bf1b1d14b1
userwebdev <webdevnan@outlook.com>
2023-06-25 19:52:34 -0700

    for deploy

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,6 +1,10 @@
 import axios from "axios";
-const BASE_URL = process.env.BASE_URL||'http://localhost:4000'
-const USER_API_URL =process.env.USER_API_URL||'http://localhost:4000/users';
+// const BASE_URL = process.env.BASE_URL||'http://localhost:4000'
+// const USER_API_URL =process.env.USER_API_URL||'http://localhost:4000/users';
+
+const BASE_URL = process.env.BASE_URL
+const USER_API_URL =process.env.USER_API_URL
+
 const api = axios.create({ withCredentials: true });
 
 
